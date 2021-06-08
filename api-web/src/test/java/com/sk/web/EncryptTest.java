92a77f3df6c76ba0a8c19fc810483df1498f71cb
“e <jinshixt@163.com>
2021-06-08 11:33:41 +0800

    增加销售记录

--- api-web/src/test/java/com/sk/web/EncryptTest.java
+++ api-web/src/test/java/com/sk/web/EncryptTest.java
@@ -1,11 +1,8 @@
 package com.sk.web;
 
-import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
